d085c3c6b71556be988ecb960c3c1509b87f0f49
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-03-12 14:40:46 +0100

    * deactivate tmpfs * move tidying up of *.pyc files from vagrant provisioning to login activity

--- scripts/bindlestiff.sh
+++ scripts/bindlestiff.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-INTERPRETERS="3.6.0 2.6.9 2.7.13 3.3.6 3.4.5 3.5.2 pypy2-5.6.0"
+# tmpfs is to small for big test runs - deactivating it to use block device
+# see https://wiki.archlinux.org/index.php/Tmpfs
+echo "===> Deactivate tmpfs"
+sudo systemctl mask tmp.mount
 
 echo "===> Install dependencies needed for compiling Python interpreters"
 sudo pacman --noconfirm -S base-devel openssl zlib git xz
@@ -12,12 +15,16 @@ echo "===> Activate pyenv ..."
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
 
-echo "===> adjust .bashrc to activate pyenv and cd into /vagrant on login"
-
+echo "===> Adjust .bashrc: activate pyenv, delete *.pyc files, cd into /vagrant on login"
 sed -i '$ a\\ export PATH="$HOME/.pyenv/bin:$PATH"' $HOME/.bashrc
 sed -i '$ a\\ eval "$(pyenv init -)"' $HOME/.bashrc
+# pytest fails with ImportMismatchError if we don't tidy up between runs
+# on the host and on the guest
+sed -i '$ a\\ echo "Removing all *.pyc files in mapped project ..."' $HOME/.bashrc
+sed -i '$ a\\ find /vagrant -name '*.pyc' -delete' $HOME/.bashrc
 sed -i '$ a\\ cd /vagrant' $HOME/.bashrc
 
+INTERPRETERS="3.6.0 2.6.9 2.7.13 3.3.6 3.4.5 3.5.2 pypy2-5.6.0"
 echo "===> Install Python versions: $INTERPRETERS"
 for version in ${INTERPRETERS}; do
     pyenv install -s ${version}
